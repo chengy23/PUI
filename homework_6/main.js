7d961ea8801d2951cfc8ace63715305f613a91c9
Yi Cheng <chengy23@uw.edu>
2018-10-24 02:42:51 -0400

    update add to cart count feature

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,3 +1,4 @@
+var count = 0;
 $(document).ready(function(){
 
   $(".qty").click(function() { 
@@ -18,8 +19,20 @@ $(document).ready(function(){
     } 
   });
 
+  //delay animation when user adds product in the shopping cart
+  function adding(){
+    $("#add_cart").css('opacity', 1);
+    $("#add_cart").text("Add to Cart");
+    $('#cart').text(count); 
+  }
 
-
+  $("#add_cart").click(function() { 
+    count++;
+    $(this).css('opacity', 0.5);
+    $(this).text("Adding..."); 
+    //visual indication that item is added to the cart
+    setTimeout(adding, 1000); 
+  });
 
 
 
@@ -52,14 +65,3 @@ $(document).ready(function(){
 
 
 
-
-function aa() {
-    var option = document.getElementsByName("optradio");
-    var len = option.length;
-
-    for (i=0; i <len; i++) {
-        if (option[i].checked) {
-            alert("check" + option[i].value);
-        }
-    }
-}
